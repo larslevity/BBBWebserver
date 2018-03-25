05ecb3e61174664d4eb252f7d16958082df7bc91
Lars Schiller <lars@familysc.de>
2018-03-25 01:59:35 +0100

    dash test works

--- dash___test.py
+++ dash___test.py
@@ -5,11 +5,12 @@ Created on Fri Mar 23 20:08:18 2018
 @author: ls
 """
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, Event
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 from collections import deque
+import time
 
 
 def flat_list(l):
@@ -18,58 +19,70 @@ def flat_list(l):
 n_sliders = 6
 max_len = 10
 
-pwm_values = {str(i): deque([0]*max_len, maxlen=max_len) for i in range(n_sliders)}
-
+pwm_values = {
+    str(i): deque([0]*max_len, maxlen=max_len) for i in range(n_sliders)}
+timestamp = {
+    str(i): deque(range(max_len), maxlen=max_len) for i in range(n_sliders)}
+timestamp['start'] = time.time()
 
 app = dash.Dash()
 
 
-sliders = [dcc.Slider(
-    id='pwm-slider-{}'.format(i),
-    min=0,
-    max=100,
-    value=0,
-    marks={str(j): str(j) for j in range(0, 101, 10)}
-) for i in range(n_sliders)]
+sliders = flat_list([[html.Label('0', id='pwm-label-{}'.format(i)),
+                      dcc.Slider(
+                            id='pwm-slider-{}'.format(i),
+                            min=0,
+                            max=100,
+                            value=0,
+                            marks={str(j): str(j) for j in range(0, 101, 10)})
+                      ] for i in range(n_sliders)])
 
 
 app.layout = html.Div(flat_list([
-    dcc.Graph(id='graph-with-slider'),
+    [dcc.Graph(id='live-graph', animate=True),
+     dcc.Interval(id='graph-update', interval=1000)],
     sliders
 ]))
 
 
-@app.callback(
-    Output('graph-with-slider', 'figure'),
-    [Input('year-slider', 'value')])
-def update_figure(selected_year):
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(go.Scatter(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers+lines',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
+for i in range(n_sliders):
+    @app.callback(Output('pwm-label-{}'.format(i), 'children'),
+                  [Input('pwm-slider-{}'.format(i), 'value')])
+    def slider_callback(val, idx=i):
+        global timestamp
+        global pwm_values
+        pwm_values[str(idx)].append(val)
+        timestamp[str(idx)].append(time.time()-timestamp['start'])
+        return val
 
+
+@app.callback(Output('live-graph', 'figure'),
+              events=[Event('graph-update', 'interval')])
+def update_graph():
+    global pwm_values
+    global timestamp
+
+    traces = []
+    minis = []
+    maxis = []
+    for key in pwm_values:
+        minis.append(min(list(timestamp[key])))
+        maxis.append(max(list(timestamp[key])))
+        data = go.Scatter(
+            x=list(timestamp[key]),
+            y=list(pwm_values[key]),
+            name=key,
+            mode='markes+lines')
+        traces.append(data)
     return {
         'data': traces,
         'layout': go.Layout(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest'
+            xaxis={'range': [min(minis), max(maxis)]},
+            yaxis={'range': [0, 100]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10}
         )
     }
 
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(host='0.0.0.0', port=5000, debug=True)
